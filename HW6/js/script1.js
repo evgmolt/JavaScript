33f080f49a5f0c56fcd1485a1253abcd05a58c6e
evgmolt <evg.molt@gmail.com>
2021-08-05 09:50:15 +0400

    Home work 6 new modal window

--- HW6/js/script1.js
+++ HW6/js/script1.js
@@ -16,6 +16,7 @@ let basket = {
         }
         return price;
     },
+    
     addProduct: function(newProduct) {
         this.goods.push(newProduct);
         var sum = this.countBasketPrice();
@@ -32,27 +33,65 @@ function addToBasket(event) {
     const targetButton = event.target;
     let index = targetButton.id;
     basket.addProduct(products[index]);
+    event.stopPropagation();
 }
 
-function showPicture(event) {
+function showImages(event) {
+    const target = event.target;
+    var number=  target.dataset.number;
+    var modal = document.querySelector("#modal"),
+    modalOverlay = document.querySelector("#modal-overlay");
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+    let images = document.getElementsByClassName("thumbnails")[0];
+    images.innerHTML = "";
+    for (var i = 0; i < 3; i++)
+    {
+        var image = document.createElement("img");
+        var image = new Image();
+        image.src = "img/IMG" + number + "_" + i + "_thumb.webp";
+        image.dataset.imageNumber = i;
+        image.dataset.productNumber = number;
+        image.addEventListener("click", openBigImage);
+        images.appendChild(image);
+        if (i === 0) {
+            image.click();
+        }
+    }
 }
+
+function openBigImage(event) {
+    const galery = document.getElementsByClassName("galery")[0];
+    galery.innerHTML = "";
+    const target = event.target;
+    var productNumber = target.dataset.productNumber;
+    var imageNumber = target.dataset.imageNumber;
+    const image = document.createElement("img");
+    
+    image.src = "img/IMG" + productNumber + "_" + imageNumber + ".webp";
+    galery.appendChild(image);
+ }
+ 
 
 function init() {
     var list = document.getElementById("productList");
     for (var i = 0; i < products.length; i++) {
         var item = document.createElement("li");
         var button = document.createElement("button");
+        item.innerHTML = products[i].name + " ";
+        item.dataset.number = i;
+        item.addEventListener('click', showImages);
         button.innerHTML = "Добавить в корзину";
         button.id = i;
-        button.addEventListener("click", addToBasket)
-        item.innerHTML = products[i].name;
-        item.addEventListener('click', showPicture);
+        button.addEventListener("click", addToBasket);
         list.appendChild(item);
         item.appendChild(button);
     }
 }
 
 window.onload = init;
+
+
 
 
 
